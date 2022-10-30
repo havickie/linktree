89436284b09fff28d9ebe98296fb042e19eada21
victor <victorfrancis612@gmail.com>
2022-10-30 19:46:36 +0100

    updated

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import '../App.css';
-import profileImage from '../img/profile-img.png'
+import profileImage from '../img/profile-img.png';
+import cam from '../img/camera.svg';
+
+
 const Profile = () => {
   return (
+    <div className='profile'>
     <div className='profile-picture'>
-      <a href='https://twitter.com/haveekk'>
+       <a href='https://twitter.com/haveekk'>
         {' '}
         <img id='profile__img' src={profileImage} alt='Profile-Picture' />
-      </a>
+       
+      </a> <div class='middle'>
+          <img src={cam} alt="" />
+        </div>
+    </div>
+     
       <div className='profile-name'>
         <h4 id='slack' className='hidden'>
           Astrocub
